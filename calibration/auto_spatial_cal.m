de0b8db2d5d21eb560129acdbd6259eb22284698
nghdavid123@gmail.com <nghdavid123@gmail.com>
2018-08-13 23:31:00 +0800

    New version

--- calibration/auto_spatial_cal.m
+++ calibration/auto_spatial_cal.m
@@ -1,25 +1,42 @@
-%% Setup video input
-vid = videoinput('gige',1,'Mono8') %Open video
-vid.SelectedSourceName = 'input1';
-scr_obj = getselectedsource(vid);
-
-set(vid, 'Exposureabs', 100);
-
-frame = getsnapshot(vid);
-image(frame);
 %% Open phychotoolbox
 Screen('Preference', 'SkipSyncTests', 1)
 global rect w
-[w, rect] = Screen('OpenWindow',2, [0 0 0]); %black background 
+[w, rect] = Screen('OpenWindow',1, [0 0 0]); %black background 
+
+
+% 
+% mea_size=433; %use odd number!
+% baseRect = [0 0 mea_size mea_size];  %use odd number!
+% baseRect = [0 0 4 4]; 
+% meaCenter_x=631; 
+% meaCenter_y=572;  
+% centeredRect = CenterRectOnPointd(baseRect, meaCenter_x, meaCenter_y);
+% Screen('FillRect', w, 255,centeredRect); 
+% Screen('Flip', w);
+
+%% Setup video input
+vid = videoinput('gige',1) %Open video
+vid.SelectedSourceName = 'input1';
+scr_obj = getselectedsource(vid);
+set(scr_obj,'GainRaw',5)
+set(scr_obj,'ExposureTimeAbs',9000000)
+
+
+
+
+frame = getsnapshot(vid);
+imshow(frame);
+
 
 
 %% Set mea parameter
 mea_size=433; %use odd number!
-cal_size = 511;%number of channels for one side, should be an odd number
+cal_size = 469;%number of channels for one side, should be an odd number
 N = 7;%
 baseRect = [0 0 mea_size mea_size];  %use odd number!
-meaCenter_x=631; %%%%%%%%%%%%Check
-meaCenter_y=605; %%%%%%%%%%%%Check 
+centeredRect = CenterRectOnPointd(baseRect, meaCenter_x, meaCenter_y);
+meaCenter_x=631; 
+meaCenter_y=572; 
 
 
 x_array=[]; y_array=[];
@@ -34,15 +51,32 @@ for i=0:cal_size-1
     y_array(1,i+1)=meaCenter_y - (cal_size-1)/2 + i;
 end
 
+% dotPositionMatrix=zeros(2,cal_size^2);
+% %put into the matrix "dotPositionMatrix"
+%  for q=0:cal_size-1
+%     for a=1:cal_size
+%         dotPositionMatrix(1,a+q*cal_size)=x_array(q+1);
+%         dotPositionMatrix(2,a+q*cal_size)=y_array(a);
+%     end
+%  end
+% 
+%  
+% 
+% for i=1:size(dotPositionMatrix,2)  
+%         baseRect = [0 0 1 1];  % one pixel
+%         xCenter=dotPositionMatrix(1,i); %determine the x coordination on monitor for this point
+%         yCenter=dotPositionMatrix(2,i);
+%         centeredRect = CenterRectOnPointd(baseRect, xCenter, yCenter);
+%         Screen('FillRect', w, dotColors, centeredRect);
+% end
+% Screen('Flip', w);
 
 
-
-
-dotColors=[255];
+tic
 %Show seven dots at one time and keep moving
-for i = 1:cal_size%x coordination 
+for i = 1%:cal_size%x coordination 
     for j = 1:cal_size/N%y coordination
-        point0 = zeros(2,N);
+        point0=zeros(2,N);
         
 %         for z = 0:N-1%Show seven points on monitor
 %             baseRect = [0 0 1 1];  % one pixel
@@ -58,32 +92,76 @@ for i = 1:cal_size%x coordination
         %Change position on the monitor
         ideal_point = zeros(2,N);
         for k = 0:N-1
-             ideal_point(1,k+1) = ideal_pt{cal_size-k*cal_size/N+j,i}(1);
-             ideal_point(2,k+1) = ideal_pt{cal_size-k*cal_size/N+j,i}(2);     
+             ideal_point(1,k+1) = ideal_pt{cal_size-(k*cal_size/N+j-1),i}(1);
+             ideal_point(2,k+1) = ideal_pt{cal_size-(k*cal_size/N+j-1),i}(2);     
         end
         
         min = ones(1,N)*10000000;
         min_point = cell(1,N);
-        point = ideal_point;
-        for z = 1:10
-            [distances,coordinates] = mindistance(point,ideal_point);
-            for i = 1:N
-                if distances(i) < min(i)
-                    min(i) = distances(i);
-                    min_point(i) = coordinates(i);
-                end
-                
-            end
-            point = coordinates;
+        for o = 1:N
+            point0(1,o) = x_array(i);
+            point0(2,o) = y_array((o-1)*cal_size/N+j);
         end
+        new_point = point0;
+        for o = 1:10
+            
+            
+            for z = 1:size(new_point,2)%Show seven points on monitor
+                baseRect = [0 0 1 1];  % one pixel
+                %centeredRect = CenterRectOnPointd(baseRect, round(ideal_point(1,z)), round(ideal_point(2,z)));
+                centeredRect = CenterRectOnPointd(baseRect, round(new_point(1,z)), round(new_point(2,z)));
+                Screen('FillRect', w, 255, centeredRect);
+            end
+            Screen('Flip', w);
+    
+            img = getsnapshot(vid);
+            detect_pt = find_detect_pt(img,N,0.95,'off',ideal_point);
+    
+            for v = 1:size(new_point,2)
+               ideal_x = ideal_point(1,v);
+               ideal_y = ideal_point(2,v);
+               detect_x = detect_pt(1,v);
+               detect_y = detect_pt(2,v);
+               if ideal_x > detect_x
+                   new_point(1,v) = new_point(1,v) +1 ;
+                   %new_point(1,v)= new_point(1,v) + round((ideal_x -detect_x)/ideal_distance_pt);
+               elseif ideal_x < detect_x
+                   new_point(1,v) = new_point(1,v) + round((ideal_x - detect_x)/ideal_distance_pt) ;
+               else
+               end
+         
+                if ideal_y > detect_y
+                    new_point(2,v) = new_point(2,v) + round((ideal_y - detect_y)/ideal_distance_pt) ;
+                elseif ideal_y < detect_y
+                    new_point(2,v) = new_point(2,v) + round((ideal_y - detect_y)/ideal_distance_pt) ;
+                else
+                end
+            end
         
+    
+            distances = zeros(1,N);
+            for u = 1:size(ideal_point,2)
+                distances(u) = norm(detect_pt(:,1)-ideal_point(:,u));
+            end
+       
+            for m = 1:N
+                if distances(m) < min(m)
+                    min(m) = distances(m);
+                    min_point{:,m} = new_point(:,m);
+                end
+
+            end
+        
+        
+        end
         
         for k = 0:N-1
-             dotPositionMatrix(y_array(k*cal_size/N+j),x_array(i)) = min_point(k+1);     
+             dotPositionMatrix{k*cal_size/N+j,i} = min_point(:,k+1);     
         end
         
-        end
+        
     end
-    end
+end
 
+toc
 save('calibirate_pt.mat','dotPositionMatrix')
--- calibration/find_detect_pt.m
+++ calibration/find_detect_pt.m
@@ -1,35 +1,47 @@
-function detect_pt = find_detect_pt(img,num_point,sensitivity,show)
+function detect_pt = find_detect_pt(img,num_point,sensitivity,show,ideal_point)
     %Function that find centers of points in the image
     CI=mat2gray(img);
-    [centers, radii, ~] = imfindcircles(CI,[1 2],'ObjectPolarity','bright','Sensitivity',sensitivity,'Method','twostage');  %find the center, radius of the detected ccd dots
+    [centers, radii, ~] = imfindcircles(CI,[2 4],'ObjectPolarity','bright','Sensitivity',sensitivity,'Method','twostage');  %find the center, radius of the detected ccd dots
     %picking the right radius size influence the detect accuracy A LOT!!
     
-    %Check the function does find correct numbers of points
-    if size(num_point,2) ~= num_point
-        error('Find not correct points')
-    end
-    
+    up_y = ideal_point(2,end);
+    down_y = ideal_point(2,1);
+    avg_x = mean(ideal_point(1,:));
+    correct_center = zeros(num_point,2);
+    correct_radius = zeros(num_point,1);
+    ii=0;
+    for i = 1:size(centers,1)
+        if centers(i,2)<down_y+100 && centers(i,2)>up_y-100 && abs(centers(i,1)-avg_x)<100
+            ii = ii+1;
+            correct_center(ii,:) = centers(i,:);
+            correct_radius(ii) = radii(i);
+        end
+        
+    end   
     %Show image
     if strcmp(show,'on')
         figure;imshow(CI); 
-        centersStrong5 = centers(1:num_point,:);
-        radiiStrong5 = radii(1:num_point);
+        centersStrong5 = correct_center;
+        radiiStrong5 = correct_radius;
         viscircles(centersStrong5, radiiStrong5,'EdgeColor','b'); %plot the detected dots image on the same picture
     end
     
-    
+    %Check the function does find correct numbers of points
+    if size(correct_center,1) ~= num_point
+        error('Find not correct points')
+    end
    
     %generate detect point cell
     %understand this code by just seeing the matrixes~
-    B = sortrows(centers);
+    B = sortrows(correct_center);
     dump = sortrows(B,2);
     B=dump;
     
     Bcell = num2cell(B,2);
-    detect_pt=zeros(2,N); %the reconstructed points
+    detect_pt=zeros(2,num_point); %the reconstructed points
     dump = [];
-    dump=Bcell(1:N,:);
-    for w=1:N
+    dump=Bcell(1:num_point,:);
+    for w=1:num_point
         detect_pt(1,w)=dump{w}(1);
         detect_pt(2,w)=dump{w}(2);
     end
