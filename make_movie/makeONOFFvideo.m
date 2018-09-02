18bd72bd86c91aa7b6dac8b93545bcd8747897c5
llincooi <33573023+llincooi@users.noreply.github.com>
2018-09-02 22:08:08 +0800

    Add files via upload

---
+++ make_movie/makeONOFFvideo.m
@@ -0,0 +1,66 @@
+%% ON OFF
+clear all;
+
+
+
+all_file = dir('*.mat');
+
+fps =60;  %freq of the screen flipping 
+T=5*60; %second
+dt=1/fps;
+T=dt:dt:T;
+
+
+
+      
+    
+cd ('/Users/nghdavid/Desktop/make_movie/OU_0819_video_Br_50')
+%video frame file
+name=['0819 OU RL G',num2str(G_OU) ,' 5min Br50 Q100'];
+name
+
+
+%video setting
+Time=T; %sec
+video_fps=fps;
+writerObj = VideoWriter([name,'.avi']);  %change video name here!
+writerObj.FrameRate = video_fps;
+writerObj.Quality = 100;
+open(writerObj);
+%start part: dark adaptation
+for mm=1:60
+    img=zeros(1024,1280);
+    writeVideo(writerObj,img);
+end
+
+%%rotation theta = 0 for RL
+
+for kk =0:length(T)-1
+    a=zeros(1024,1280);%full screen pixel matrix %it's the LED screen size
+    
+    %square_flicker
+    if mod(kk,10)<5 %odd number
+        a=ones(1024,1280); % white square
+    elseif mod(kk,10)>=5
+        a=zeros(1024,1280); % dark
+    else
+        
+    end
+    
+    
+    writeVideo(writerObj,a);
+end
+
+%end part video
+for mm=1:10
+    img=zeros(1024,1280);
+    img(500-35:500+35,1230:1280)=0.2; %gray
+    writeVideo(writerObj,img);
+end
+close(writerObj);
+cd('/Users/nghdavid/Desktop/make_movie/OU_0819_video_Br_50_workspace')
+%save parameters needed
+save(['0819 OU RL G',num2str(G_OU) ,' 5min Br50 Q100','.mat'],'new_x')
+
+
+cd('/Users/nghdavid/Desktop/make_movie')
