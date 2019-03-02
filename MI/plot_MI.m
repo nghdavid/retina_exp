e2ba962da1a91038072af3b5b0090f12fc650f33
nghdavid <35591407+nghdavid@users.noreply.github.com>
2019-03-02 13:56:58 +0800

    Modify code for MI

---
+++ MI/plot_MI.m
@@ -0,0 +1,21 @@
+for channelnumber=1:60
+figure;
+plot(time,Mutual_infos{channelnumber}-Mutual_shuffle_infos{channelnumber}); hold on; %,'color',cc(z,:));hold on
+
+xlabel('delta t (ms)');ylabel('MI (bits/second)( minus shuffled)');
+set(gca,'fontsize',12); hold on
+
+ 
+
+xlim([ -15000 15000])
+ylim([0 inf])
+title(['channel ',num2str(channelnumber)]) 
+
+
+
+end
+
+
+
+
+
