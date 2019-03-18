7d5cb047059406125ad2817ab20e14d6150b4cdf
llincooi <33573023+llincooi@users.noreply.github.com>
2019-03-18 17:38:06 +0800

    bar

--- MI/thestimuli.m
+++ MI/thestimuli.m
@@ -1,23 +1,32 @@
-%absulote pos
-TheStimuli=bin_pos;  %recalculated bar position
+function TheStimuli = thestimuli(stimuli_type, video_type, channel_num) 
 
-%velocity
-TheStimuli = [];
-TheStimuli(1) = 0;
-TheStimuli(length(bin_pos)) = 0;
-for i =2:(length(bin_pos)-1)
-    TheStimuli(i) = ((bin_pos(i+1) + bin_pos(i))/2 - (bin_pos(i-1) + bin_pos(i))/2)*60/1000;
-end
+if stimuli_type == 'pos'%absulote pos
+    TheStimuli=bin_pos;  %recalculated bar position
     
-%distance
-meaCenter_x=715; 
-meaCenter_y=634; 
-if type = "RL"
-    TheStimuli= channel_pos(1) - bin_pos;
-elseif type = "UD"
-    TheStimuli= (channel_pos(2)-meaCenter_y) - (bin_pos-meaCenter_x);
-elseif type = "UL-DR"
-    TheStimuli= (channel_pos(1)-channel_pos(2)+meaCenter_y-meaCenter_x)/sqrt(2) - (bin_pos-meaCenter_x);
-else 
-    TheStimuli= (-channel_pos(1)-channel_pos(2)+meaCenter_y+meaCenter_x)/sqrt(2) - (bin_pos-meaCenter_x);
+elseif stimuli_type == 'vel'%velocity
+    
+    TheStimuli = [];
+    TheStimuli(1) = 0;
+    TheStimuli(length(bin_pos)) = 0;
+    for i =2:(length(bin_pos)-1)
+        TheStimuli(i) = ((bin_pos(i+1) + bin_pos(i))/2 - (bin_pos(i-1) + bin_pos(i))/2)*60/1000;
+    end
+    
+elseif stimuli_type == 'bar_loc' % if the bar locate at the cannel or not (bool)
+    
+    load('boundary_set.mat')
+    load('channel_pos.mat')
+    
+    if video_type == "RL"
+        TheStimuli = abs(channel_pos(channel_num,1) - bin_pos) <= bar_wid && abs(channel_pos(channel_num,2) - meaCenter_y) <= bar_le ;
+    elseif video_type == "UD"
+        TheStimuli= abs((channel_pos(channel_num,2)-meaCenter_y) - (bin_pos-meaCenter_x)) <= bar_wid && abs(channel_pos(channel_num,1) - meaCenter_x) <= bar_le ;
+    elseif video_type == "UL-DR"
+        TheStimuli= abs((channel_pos(channel_num,1)+channel_pos(channel_num,2)-meaCenter_y-meaCenter_x)/sqrt(2) - (bin_pos-meaCenter_x)) <= bar_wid && abs((-channel_pos(channel_num,1)+channel_pos(channel_num,2)-meaCenter_y+meaCenter_x)/sqrt(2)) <= bar_le;
+    else
+        TheStimuli= abs((-channel_pos(channel_num,1)+channel_pos(channel_num,2)-meaCenter_y+meaCenter_x)/sqrt(2) - (bin_pos-meaCenter_x)) <= bar_wid && abs((channel_pos(channel_num,1)+channel_pos(channel_num,2)-meaCenter_y-meaCenter_x)/sqrt(2)) <= bar_le;
+    end
+else
+end
+
 end
