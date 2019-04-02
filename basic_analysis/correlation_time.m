2a2dd51124a01306f9d25c2b4d6dfd94317b683a
nghdavid <35591407+nghdavid@users.noreply.github.com>
2019-04-02 23:30:50 +0800

    correlation time

---
+++ basic_analysis/correlation_time.m
@@ -0,0 +1,5 @@
+%% Code that calculate correlation time
+%load merge data first
+acf = autocorr(bin_pos,'NumLags',100);
+corr_time = find(abs(acf-0.5) ==min(abs(acf-0.5)))/60;
+disp(['The correlation time is ',num2str(corr_time),' second'])
