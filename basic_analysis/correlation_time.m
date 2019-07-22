88f6443a53292f31c85add58046e1c53b49d843a
nghdavid <35591407+nghdavid@users.noreply.github.com>
2019-07-22 14:05:34 +0800

    Update grating parameter

--- basic_analysis/correlation_time.m
+++ basic_analysis/correlation_time.m
@@ -1,5 +1,5 @@
 %% Code that calculate correlation time
 %load merge data first
-acf = autocorr(bin_pos,'NumLags',100);
+acf = autocorr(bin_pos,100);
 corr_time = find(abs(acf-0.5) ==min(abs(acf-0.5)))/60;
 disp(['The correlation time is ',num2str(corr_time),' second'])
