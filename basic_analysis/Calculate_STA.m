e24c751559faea6cf02c39eb850cfddc32c0775d
nghdavid <35591407+nghdavid@users.noreply.github.com>
2019-08-10 19:24:16 +0800

    Add code for calculating distance STA

---
+++ basic_analysis/Calculate_STA.m
@@ -0,0 +1,52 @@
+%% This code calculate HMM and OU bar position STA
+close all;
+clear all;
+code_folder = pwd;
+exp_folder = 'E:\0709';
+cd(exp_folder);
+mkdir STA
+cd sort_merge_spike\MI
+all_file = subdir('*.mat') ; % change the type of the files which you want to select, subdir or dir.
+n_file = length(all_file) ;
+
+forward = 90;%90 bins before spikes for calculating STA
+backward = 90;%90 bins after spikes for calculating STA
+
+for z =1:n_file %choose file
+    
+    file = all_file(z).name ;
+    [pathstr, name, ext] = fileparts(file);
+    directory = [pathstr,'\'];
+    filename = [name,ext];
+    load(filename);
+    name=[name];
+    z
+    name
+    
+     % put your stimulus here!!
+    TheStimuli=relative_pos;  %recalculated bar position
+    time=[-forward :backward]*BinningInterval;
+    % Binning
+    bin=BinningInterval*10^3; %ms
+    BinningTime =diode_BT;
+    
+    %% BinningSpike and calculate STA
+    BinningSpike = zeros(60,length(BinningTime));
+    sum_n = zeros(1,60);
+    dis_STA = zeros(60,forward+backward+1);
+    for i = 1:60  % i is the channel number
+        if sum(TheStimuli(i,:)) <= 0
+            disp(['channel ',int2str(i),'does not have RF center'])
+            continue
+        end
+        [n,~] = hist(sorted_spikes{i},BinningTime) ;  
+        BinningSpike(i,:) = n ;
+        sum_n(i) = sum_n(i)+ sum(BinningSpike(i,forward+1:length(BinningTime)-backward));
+        for time_shift = forward+1:length(BinningTime)-backward
+            dis_STA(i,:) = dis_STA(i,:) + BinningSpike(i,time_shift)*TheStimuli(i,time_shift-forward:time_shift+backward);
+        end
+        dis_STA(i,:) = dis_STA(i,:)/sum_n(i);
+    end
+    save([exp_folder,'\STA\',name(17:end),'.mat'],'time','dis_STA')
+end
+
---
+++ basic_analysis/plot_STA.m
@@ -0,0 +1,57 @@
+close all;
+clear all;
+code_folder = pwd;
+load('boundary_set.mat')
+exp_folder = 'E:\0709';
+cd(exp_folder)
+cd STA
+mkdir FIG
+display_channel = [1:60];
+%Tina orientation
+rr =[9,17,25,33,41,49,...
+          2,10,18,26,34,42,50,58,...
+          3,11,19,27,35,43,51,59,...
+          4,12,20,28,36,44,52,60,...
+          5,13,21,29,37,45,53,61,...
+          6,14,22,30,38,46,54,62,...
+          7,15,23,31,39,47,55,63,...
+            16,24,32,40,48,56];
+all_file = dir('*.mat') ; % change the type of the files which you want to select, subdir or dir. 
+n_file = length(all_file) ;        
+for z =1:n_file
+    file = all_file(z).name ;
+    [pathstr, name, ext] = fileparts(file);
+    directory = [pathstr,'\'];
+    filename = [name,ext];
+    load([filename]);
+    figure('units','normalized','outerposition',[0 0 1 1])
+    ha = tight_subplot(8,8,[.04 .02],[0.07 0.02],[.02 .02]);
+    for channelnumber=1:60
+        if sum(dis_STA(channelnumber,:)) <= 0
+            disp(['channel ',int2str(channelnumber),' does not have RF center'])
+            continue
+        end
+        axes(ha(rr(channelnumber)));
+        plot(time,dis_STA(channelnumber,:)*micro_per_pixel,'LineWidth',2,'LineStyle','-');hold on;
+        grid on
+        %Y axis is um
+        title(channelnumber)
+    end
+    set(gcf,'units','normalized','outerposition',[0 0 1 1])
+    fig = gcf;
+    fig.PaperPositionMode = 'auto';
+    saveas(fig,['FIG\',name,'.tiff'])
+    saveas(gcf,['FIG\',name,'.fig'])
+end
+% 
+%  for i = display_channel  % i is the channel number
+%       if sum(TheStimuli(i,:)) <= 0
+%             disp(['channel ',int2str(i),'does not have RF center'])
+%             continue
+%     end
+%     figure(i)
+%     plot(time,dis_STA(i,:)*micro_per_pixel)
+%     title(['channel ',int2str(i),' distance STA'])
+%     xlabel('time')
+%     ylabel('relative position (um)')
+%  end
