15b4a9fa76555d4e25f3b780a55c487444efe1b4
nghdavid <35591407+nghdavid@users.noreply.github.com>
2019-08-11 11:07:09 +0800

    Modify MI that can calculate absolute MI

--- MI/Calculate_MI.m
+++ MI/Calculate_MI.m
@@ -1,10 +1,11 @@
 clear all;
 code_folder = pwd;
-sorted = 1;
+sorted =1;
 exp_folder = 'E:\0709';
 cd(exp_folder);
 mkdir MI
 cd MI
+absolute = 1;
 if sorted
     mkdir sort
     cd ([exp_folder,'\sort_merge_spike\MI'])
@@ -18,7 +19,7 @@ else
 end
 cd(code_folder);
 
-for z =1:n_file %choose file
+for z =1%:n_file %choose file
     Mutual_infos = cell(1,60);
     Mutual_shuffle_infos = cell(1,60);
     
@@ -33,22 +34,40 @@ for z =1:n_file %choose file
 
 
     % put your stimulus here!!
-    TheStimuli=bin_pos;  %recalculated bar position
+    if absolute
+        TheStimuli=absolute_pos;  %recalculated bar position
+    else
+        TheStimuli=bin_pos;  %recalculated bar position
+    end
 
     % Binning
     bin=BinningInterval*10^3; %ms
     BinningTime =diode_BT;
 
     StimuSN=30; %number of stimulus states
-    nX=sort(TheStimuli);
-    abin=length(nX)/StimuSN;
-    intervals=[nX(1:abin:end) inf]; % inf: the last term: for all rested values
-    temp=0; isi2=[];
-    for jj=1:length(TheStimuli)
-        temp=temp+1;
-        isi2(temp) = find(TheStimuli(jj)<=intervals,1);
-    end 
-
+    if absolute
+         nX=sort(TheStimuli,2);
+        abin=length(nX)/StimuSN;
+        
+        isi2 = zeros(60,length(TheStimuli));
+        for k = 1:60
+            if sum(absolute_pos(k,:))>0
+                intervals=[nX(k,1:abin:end) inf]; % inf: the last term: for all rested values
+                for jj=1:length(TheStimuli)
+                    isi2(k,jj) = find(TheStimuli(k,jj)<=intervals,1);
+                end
+            end
+        end
+    else
+        nX=sort(TheStimuli);
+        abin=length(nX)/StimuSN;
+        intervals=[nX(1:abin:end) inf]; % inf: the last term: for all rested values
+        temp=0; isi2=[];
+        for jj=1:length(TheStimuli)
+            temp=temp+1;
+            isi2(temp) = find(TheStimuli(jj)<=intervals,1);
+        end 
+    end
     %% BinningSpike
     BinningSpike = zeros(60,length(BinningTime));
     if sorted
@@ -70,8 +89,17 @@ for z =1:n_file %choose file
     for channelnumber=1:60
 
         Neurons = BinningSpike(channelnumber,:);  %for single channel
-        information = MIfunc(Neurons,isi2,BinningInterval,backward,forward);
-        
+        if absolute
+            if sum(absolute_pos(channelnumber,:))>0
+             information = MIfunc(Neurons,isi2(channelnumber,:),BinningInterval,backward,forward);
+            else
+                Mutual_infos{channelnumber} = zeros(1,length(time));
+                Mutual_shuffle_infos{channelnumber} =zeros(1,length(time));;
+                continue
+            end
+        else
+             information = MIfunc(Neurons,isi2,BinningInterval,backward,forward);
+        end
         %% shuffle MI
         sNeurons=[];
         r=randperm(length(Neurons));
@@ -79,17 +107,28 @@ for z =1:n_file %choose file
             sNeurons(j)=Neurons(r(j));
         end
         Neurons_shuffle=sNeurons;
-
-        information_shuffle = MIfunc(Neurons_shuffle,isi2,BinningInterval,backward,forward);
-
+        
+        if absolute
+            information_shuffle = MIfunc(Neurons_shuffle,isi2(channelnumber,:),BinningInterval,backward,forward);
+        else
+            information_shuffle = MIfunc(Neurons_shuffle,isi2,BinningInterval,backward,forward);
+        end
         
         Mutual_infos{channelnumber} = information;
         Mutual_shuffle_infos{channelnumber} = information_shuffle;
 
     end
-    if sorted
-        save([exp_folder,'\MI\sort\',name(12:end),'.mat'],'time','Mutual_infos','Mutual_shuffle_infos')
+    if absolute 
+         if sorted
+            save([exp_folder,'\MI\sort\absolute_',name(12:end),'.mat'],'time','Mutual_infos','Mutual_shuffle_infos')
+        else
+            save([exp_folder,'\MI\unsort\absolute_',name(7:end),'.mat'],'time','Mutual_infos','Mutual_shuffle_infos')
+        end
     else
-        save([exp_folder,'\MI\unsort\',name(7:end),'.mat'],'time','Mutual_infos','Mutual_shuffle_infos')
+        if sorted
+            save([exp_folder,'\MI\sort\',name(12:end),'.mat'],'time','Mutual_infos','Mutual_shuffle_infos')
+        else
+            save([exp_folder,'\MI\unsort\',name(7:end),'.mat'],'time','Mutual_infos','Mutual_shuffle_infos')
+        end
     end
 end
