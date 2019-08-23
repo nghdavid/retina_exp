00670a8f8ca66fa4f8d5e2be3ba057511af969bf
nghdavid <35591407+nghdavid@users.noreply.github.com>
2019-08-23 14:09:02 +0800

    Modify HMM movie for seed and length

--- make_movie/fucntionalized/MakeMovie.m
+++ make_movie/fucntionalized/MakeMovie.m
@@ -1,29 +1,30 @@
 clear all;
 makemovie_folder = 'E:\makemovie';
-date = '0820';
+date = '0319';
+seed_date = '0421';
 cd(makemovie_folder);
 load('calibrate_pt.mat')%Load dotPositionMatrix
 load('screen_brightness.mat')%Load screen_brightness
 load('boundary_set.mat')
-% 1
+% 
 % makeGratingvideo(makemovie_folder, 300, 'D:\0304v\videos', 'D:\0304v\videoworkspace', date);
 %makeTestHMMvideo(makemovie_folder, 0, 'RL', 'D:\0304v\videos\HMM', 'C:\Users\hydro_leo\Desktop\0304v\videoworkspace\HMM', date);
-% makeHMMvideo(makemovie_folder, 0, 'RL', 'D:\0304v\videos\HMM', 'D:\0304v\videoworkspace\HMM', date);
-% makeHMMvideo(makemovie_folder, pi/2, 'UD', 'D:\0304v\videos\HMM', 'D:\0304v\videoworkspace\HMM', date);
-% makeHMMvideo(makemovie_folder, pi/4, 'UL_DR', 'D:\0304v\videos\HMM', 'D:\0304v\videoworkspace\HMM', date);
-% makeHMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', 'D:\0304v\videos\HMM', 'D:\0304v\videoworkspace\HMM', date);
+makeHMMvideo(makemovie_folder, 0, 'RL', 'D:\0304v\videos\HMM', 'D:\0304v\videoworkspace\HMM', seed_date,date,5);
+makeHMMvideo(makemovie_folder, pi/2, 'UD', 'D:\0304v\videos\HMM', 'D:\0304v\videoworkspace\HMM', seed_date,date,5);
+makeHMMvideo(makemovie_folder, pi/4, 'UL_DR', 'D:\0304v\videos\HMM', 'D:\0304v\videoworkspace\HMM', seed_date,date,5);
+makeHMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', 'D:\0304v\videos\HMM', 'D:\0304v\videoworkspace\HMM', seed_date,date,5);
 
 
-%makeDarkHMMvideo(makemovie_folder, pi/2, 'UD', 'E:\', 'D:\0304v\videoworkspace\HMM', date);
-%makeDarkHMMvideo(makemovie_folder, pi/4, 'UL_DR', 'E:\', 'D:\0304v\videoworkspace\HMM', date);
-%makeDarkHMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', 'E:\', 'D:\0304v\videoworkspace\HMM', date);
-%makeDarkHMMvideo(makemovie_folder, 0, 'RL', 'E:\', 'D:\0304v\videoworkspace\HMM', date);
+makeDarkHMMvideo(makemovie_folder, pi/2, 'UD', 'E:\', 'D:\0304v\videoworkspace\HMM', seed_date,date,5);
+makeDarkHMMvideo(makemovie_folder, pi/4, 'UL_DR', 'E:\', 'D:\0304v\videoworkspace\HMM', seed_date,date,5);
+makeDarkHMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', 'E:\', 'D:\0304v\videoworkspace\HMM', seed_date,date,5);
+makeDarkHMMvideo(makemovie_folder, 0, 'RL', 'E:\', 'D:\0304v\videoworkspace\HMM', seed_date,date,5);
 % makeOUvideo(makemovie_folder, 0, 'RL', 'D:\0304v\videos\OU\RL', 'C:\Users\hydro_leo\Desktop\0304v\videoworkspace\OU', date);
 % makeOUvideo(makemovie_folder, pi/2, 'UD', 'D:\0304v\videos\OU\UD', 'C:\Users\hydro_leo\Desktop\0304v\videoworkspace\OU', date);
 % makeOUvideo(makemovie_folder, pi/4, 'UL_DR', 'D:\0304v\videos\OU\UL_DR', 'C:\Users\hydro_leo\Desktop\0304v\videoworkspace\OU', date);
 % makeOUvideo(makemovie_folder, 3*pi/4, 'UR_DL', 'D:\0304v\videos\OU\UR_DL', 'C:\Users\hydro_leo\Desktop\0304v\videoworkspace\OU', date);
 % 
-Gollish_OnOff_movie(makemovie_folder, 'D:\0304v\videos',  date);
+%Gollish_OnOff_movie(makemovie_folder, 'D:\0304v\videos',  date);
 % makeCirclevideo(makemovie_folder, 'D:\0304v\videos', 'C:\Users\hydro_leo\Desktop\0304v\videoworkspace', date);
 % checkerboard(makemovie_folder,  'D:\0304v\videos','D:\0304v\videoworkspace',  date);
 % OnOffvideo(makemovie_folder, 'D:\0304v\videos',  date);
