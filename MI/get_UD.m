7b43d2ea697edbf550a4c6ddef77f1c9933ec844
llincooi <33573023+llincooi@users.noreply.github.com>
2019-08-26 14:40:53 +0800

    get_row_spike

--- MI/get_UD.m
+++ MI/get_UD.m
@@ -2,20 +2,20 @@ function UD_spikes = get_UD(BinningSpike)
 %%Function that get sum of BinningSpike for calculating MI of UD
 %UD_spikes{1} is up
 %UD_spikes{8} is down
-    UD_spikes = cell(1,8);
+    UD_spikes = zeros(8,length(BinningSpike));
 
     %Up and down part
     for i = [7 14]
-       total_spikes = zeros(6,length(bin_pos));
+       total_spikes = zeros(6,length(BinningSpike));
        for j = 1:6
           total_spikes(j,:) = BinningSpike(i+(j-1)*8,:);
        end
-       UD_spikes{i-6} = sum(total_spikes,1);
+       UD_spikes(i-6, :) = sum(total_spikes,1);
     end
 
     %middle part
     for i = 1:6
-       total_spikes = zeros(8,length(bin_pos));
+       total_spikes = zeros(8,length(BinningSpike));
        start = 7+(i-2)*8;
 
        total_spikes(1,:) = BinningSpike(i,:);
@@ -25,7 +25,7 @@ function UD_spikes = get_UD(BinningSpike)
           total_spikes(j,:) = BinningSpike(i+7+(j-2)*8,:);
        end
 
-       UD_spikes{i+1} = sum(total_spikes,1);
+       UD_spikes(i+1, :) = sum(total_spikes,1);
     end
 
 end
