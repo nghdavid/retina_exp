fe885346064d707e4a8ec8797dcde0dc2b26b5dd
nghdavid <35591407+nghdavid@users.noreply.github.com>
2019-09-13 17:35:19 +0800

    Upload final version of STA

--- ploting/plot_different_STA.m
+++ ploting/plot_different_STA.m
@@ -1,28 +1,34 @@
 close all;
 clear all;
 code_folder = pwd;
-exp_folder = 'E:\20190825';
+exp_folder = 'E:\20190811';
 cd(exp_folder)
+load('rr.mat')
 load('Analyzed_data\RFcenter.mat')%Needed to run Receptive field.m first
 direction = 'RL';
 HMM_different_G = [3,4.5,7.5,12,20];
-mins = 7;
-date = '0602';
-order = '2';
+mins = 7;%Length of movie length
+date = '0602';%Date of movie
+order = '1';%First or second experiment
 sorted=1;
-save_photo = 1;
+save_photo = 0;
 OU_different_G = [2.45,10.5];
 OU_or_Not = 1;%Plot OU or not
-all_or_Not = 1;%Plot all channels or not
-predicitive_or_Not = 1;
+all_or_Not = 0;%Plot all channels or not
 HMM_former_name = [date,'_HMM_',direction,'_G'];
 HMM_post_name = ['_',num2str(mins),'min_Br50_Q100_',order];
-HMM_filename =  [date,'_HMM_',direction,'_',num2str(length(HMM_different_G )),'G_',num2str(mins),'min_Br50_Q100_',order,'_ch'];     
-OU_filename =  [date,'_HMMandOU_',direction,'_',num2str(length(HMM_different_G )),'G_',num2str(mins),'min_Br50_Q100_',order,'_ch'];   
+HMM_filename =  [date,'_HMM_',direction,'_',num2str(length(HMM_different_G )),'G_',num2str(mins),'min_Br50_Q100_',order,'_ch']; %Filename used to save(Plot HMM only)   
+OU_filename =  [date,'_HMMandOU_',direction,'_',num2str(length(HMM_different_G )),'G_',num2str(mins),'min_Br50_Q100_',order,'_ch'];%Filename used to save(Plot HMM and OU)   
 OU_former_name = ['0319_OU_',direction,'_G'];
 OU_post_name = '_5min_Br50_Q100';
 
-%% Load predictive or nonpredictive channel
+allchannellegend = cell(1,length(HMM_different_G));%Save which G
+corr_t_legend = cell(1,length(HMM_different_G));%Save HMM correlation time
+all_corr_t_legend = cell(1,length(HMM_different_G)+length(OU_different_G));%Save HMM and OU correlation time
+%% Load predictive or nonpredictive channel and set roi
+%If you don't have saved this data, just comment these sentences
+%And set roi for yourself
+predicitive_or_Not = 0;
 load(['predictive_channel\',HMM_filename(1:end-3),'.mat'])
 if predicitive_or_Not
     roi = p_channel;
@@ -30,8 +36,9 @@ else
     roi = np_channel;
 end
 
+%Set roi
+% roi = [5,6,13,52,59];%Channel you want to display
 
-% roi = [5,6,13,52,59];
 %% Load STA data
 if sorted
     cd STA
@@ -40,27 +47,23 @@ else
     cd STA
     mkdir FIG
 end
-rr =[9,17,25,33,41,49,...
-          2,10,18,26,34,42,50,58,...
-          3,11,19,27,35,43,51,59,...
-          4,12,20,28,36,44,52,60,...
-          5,13,21,29,37,45,53,61,...
-          6,14,22,30,38,46,54,62,...
-          7,15,23,31,39,47,55,63,...
-            16,24,32,40,48,56];
 
+%Load HMM STA data and correlation time
 HMM_STA =[];
-
 for  G =1:length(HMM_different_G) 
     load([HMM_former_name,num2str(HMM_different_G(G)),HMM_post_name ,'.mat'])
     HMM_STA = [HMM_STA;dis_STA];
+    allchannellegend{G} = ['G', num2str(HMM_different_G(G))];
+    all_corr_t_legend{G} = [num2str(corr_time),' sec'];
+    corr_t_legend{G} = [num2str(corr_time),' sec'];
 end
-
+%Load OU STA data and correlation time
 if OU_or_Not
     OU_STA =[];
     for   G =1:length(OU_different_G) 
         load([OU_former_name,num2str(OU_different_G(G)),OU_post_name ,'.mat'])
         OU_STA = [OU_STA;dis_STA];
+        all_corr_t_legend{G+length(HMM_different_G)} = ['OU-', num2str(corr_time),' sec'];
     end
 end
 
@@ -68,13 +71,13 @@ end
 if all_or_Not
     figure('units','normalized','outerposition',[0 0 1 1])
     ha = tight_subplot(8,8,[.04 .02],[0.07 0.02],[.02 .02]);
-
-
     for channelnumber=1:60 %choose file
         axes(ha(rr(channelnumber))); 
+        %Plot HMM
         for G =1:length(HMM_different_G)
-            plot(time,HMM_STA ((G-1)*60+channelnumber,:)); hold on; %,'color',cc(z,:));hold on
+            plot(time,HMM_STA ((G-1)*60+channelnumber,:)); hold on;
         end
+        %Plot OU 
         if OU_or_Not
          for   G =1:length(OU_different_G)
             plot(time,OU_STA ((G-1)*60+channelnumber,:),'-.'); hold on;
@@ -86,6 +89,7 @@ if all_or_Not
         title(channelnumber)
         hold off;
     end
+    %Save fig
     set(gcf,'units','normalized','outerposition',[0 0 1 1])
     fig =gcf;
     fig.PaperPositionMode = 'auto';
@@ -102,7 +106,8 @@ end
 %% Plot single channel
 for channelnumber= roi 
      figure(channelnumber)
-     for   G =1:length(HMM_different_G)
+     for G =1:length(HMM_different_G)
+        %Plot HMM with different line
         if G >=4
             plot(time,HMM_STA ((G-1)*60+channelnumber,:)); hold on;
         elseif G >2
@@ -119,22 +124,31 @@ for channelnumber= roi
      grid on
      
      if OU_or_Not
-        lgd =legend('1.08sec','0.8sec','0.4833sec','0.32sec','0.18sec','OU-1.1167sec','OU-0.27sec','Location','northwest');
+       lgd =legend(all_corr_t_legend,'Location','northwest');
      else
-        lgd =legend('1.08sec','0.8sec','0.4833sec','0.32sec','0.18sec','Location','northwest');
+       lgd =legend(corr_t_legend,'Location','northwest');
      end
      lgd.FontSize = 11;
      legend('boxoff')
-     title(channelnumber)
+     %Label predicitive or not
+     if exist('predicitive_or_Not') 
+          if predicitive_or_Not%predictive channel
+              title(['p',int2str(channelnumber)])
+          else%non-predictive channel
+              title(['np',int2str(channelnumber)])
+          end
+     else%If does not have predictive channel
+          title(channelnumber)
+     end
      xlabel('time relative to spike (second)')
-     
      if sum(RFcenter(channelnumber,:)) > 0%If has RF center, it will plot different color
         set(gca,'Color',[0.9 0.9 0.9])
         ylabel('Distance relative to RF center')
-     else
+     else%Does not have RF center
         ylabel('Distance relative to electrode')
      end
      hold off;
+     %Save photo
      set(gcf,'units','normalized','outerposition',[0 0 1 1])
      fig =gcf;
      fig.PaperPositionMode = 'auto';
