3011a6d984c1d979861fb0b360017a09488111fd
nghdavid <35591407+nghdavid@users.noreply.github.com>
2019-09-15 16:00:08 +0800

    Update for pos&v and also for Leo name method

--- ploting/MI_allchannelplot.m
+++ ploting/MI_allchannelplot.m
@@ -1,7 +1,8 @@
 close all;
 clear all;
+load('rr.mat')
 code_folder = pwd;
-exp_folder = 'E:\20190811';
+exp_folder = 'E:\20190709';
 cd(exp_folder)
 sorted =1;
 %Load calculated MI first(Need to run Calculate_MI.m first to get)
@@ -12,18 +13,8 @@ else
 end
 mkdir FIG
 all_file = dir('*.mat') ; % change the type of the files which you want to select, subdir or dir. 
-n_file = length(all_file) ;
-%Tina orientation
-rr =[9,17,25,33,41,49,...
-          2,10,18,26,34,42,50,58,...
-          3,11,19,27,35,43,51,59,...
-          4,12,20,28,36,44,52,60,...
-          5,13,21,29,37,45,53,61,...
-          6,14,22,30,38,46,54,62,...
-          7,15,23,31,39,47,55,63,...
-            16,24,32,40,48,56];
-        
-for z =18%1:n_file
+n_file = length(all_file) ;        
+for z =1:n_file
     file = all_file(z).name ;
     [pathstr, name, ext] = fileparts(file);
     directory = [pathstr,'\'];
@@ -31,43 +22,13 @@ for z =18%1:n_file
     load([filename]);
     figure('units','normalized','outerposition',[0 0 1 1])
     ha = tight_subplot(8,8,[.04 .02],[0.07 0.02],[.02 .02]);
-    MI_peak=zeros(1,60);
-    ind_peak=zeros(1,60);
-    peak_times = zeros(1,60)-1000000;
-    P_channel = [];
-    N_channel = [];
-    
+    set(ha, 'Visible', 'off');
     for channelnumber=1:60
         if max(Mutual_infos{channelnumber}-mean(Mutual_shuffle_infos{channelnumber}))<0.1
             continue;
         end
         Mutual_infos{channelnumber} = smooth(Mutual_infos{channelnumber});
-        [MI_peak(channelnumber), ind_peak(channelnumber)] = max(Mutual_infos{channelnumber}); % MI_peak{number of data}{number of channel}
-        if (time(ind_peak(channelnumber)) < -1000) || (time(ind_peak(channelnumber)) > 1000) % the 100 points, timeshift is 1000
-            MI_peak(channelnumber) = NaN;
-            ind_peak(channelnumber) = NaN;
-        end
-    % ======= exclude the MI that is too small ===========
-        
-       
-       
-        pks_1d=ind_peak(channelnumber);
-        peaks_ind=pks_1d(~isnan(pks_1d));
-        peaks_time = time(peaks_ind); 
-        % ============ find predictive cell=============
-
-        
-       
-        if peaks_time>=0
-            P_channel=[P_channel channelnumber];
-        elseif peaks_time<0
-             N_channel=[N_channel channelnumber];
-        end
-        if length(peaks_time)>0
-            peak_times(channelnumber) = peaks_time;
-        end
         axes(ha(rr(channelnumber))); 
-        
         plot(time,Mutual_infos{channelnumber},'LineWidth',2,'LineStyle','-');hold on;
         plot(time,smooth(Mutual_shuffle_infos{channelnumber}),'LineWidth',2,'LineStyle','-');hold off;
          if ismember(channelnumber,P_channel)
@@ -88,11 +49,7 @@ for z =18%1:n_file
     fig.InvertHardcopy = 'off';
     saveas(fig,['FIG\',name,'.tif'])
     saveas(fig,['FIG\',name,'.fig'])
-    if sorted
-            save([exp_folder,'\MI\sort\',filename],'time','Mutual_infos','Mutual_shuffle_infos','P_channel','N_channel','peak_times')
-    else
-            save([exp_folder,'\MI\unsort\',filename],'time','Mutual_infos','Mutual_shuffle_infos','P_channel','N_channel','peak_times')
-    end
+
     close(gcf)
 end
 
--- ploting/plot_differentG.m
+++ ploting/plot_differentG.m
@@ -1,111 +1,169 @@
 close all;
 clear all;
+load('rr.mat')
 code_folder = pwd;
-exp_folder = 'D:\Leo\0807exp';
+exp_folder = 'E:\20190721';
 cd(exp_folder)
-HMM_former_name = 'pos_0319_HMM_UL_DR_G';
-HMM_post_name = '_7min_Br50_Q100';
-HMM_different_G = [2.5,4.5,9,12,20];
+type = 'v';
+direction = 'UD';
+HMM_different_G = [3,4.5,7.5,12,20];
+mins = 7;%Length of movie length
+date = '0602';%Date of movie
+order = '1';%First or second experiment
+sorted=1;
+save_photo = 1;
+OU_different_G = [2.45,10.5];
+OU_or_Not = 1;%Plot OU or not
+all_or_Not = 1;%Plot all channels or not
+roi = [];
+HMM_former_name = [type,'_',date,'_HMM_',direction,'_G'];
+HMM_post_name = ['_',num2str(mins),'min_Br50_Q100_',order];
+HMM_filename =  [type,'_',date,'_HMM_',direction,'_',num2str(length(HMM_different_G )),'G_',num2str(mins),'min_Br50_Q100_',order,'_ch']; %Filename used to save(Plot HMM only)   
+OU_filename =  [type,'_',date,'_HMMandOU_',direction,'_',num2str(length(HMM_different_G )),'G_',num2str(mins),'min_Br50_Q100_',order,'_ch'];%Filename used to save(Plot HMM and OU)   
+OU_former_name = [type,'_','0319_OU_',direction,'_G'];
+OU_post_name = '_5min_Br50_Q100';%Load calculated MI first(Need to run Calculate_MI.m first to get)
+if sorted
+    cd MI\sort
+else
+    cd MI\unsort
+end
 
+%% Load data
+allchannellegend = cell(1,length(HMM_different_G));%Save which G
+corr_t_legend = cell(1,length(HMM_different_G));%Save HMM correlation time
+all_corr_t_legend = cell(1,length(HMM_different_G)+length(OU_different_G));%Save HMM and OU correlation time
 
-OU_former_name = 'pos_0319_OU_UL_DR_G';
-OU_post_name = '_5min_Br50_Q100';
-OU_different_G = [10.5];
-
-%Load calculated MI first(Need to run Calculate_MI.m first to get)
-cd MI\unsort
-rr =[9,17,25,33,41,49,...
-    2,10,18,26,34,42,50,58,...
-    3,11,19,27,35,43,51,59,...
-    4,12,20,28,36,44,52,60,...
-    5,13,21,29,37,45,53,61,...
-    6,14,22,30,38,46,54,62,...
-    7,15,23,31,39,47,55,63,...
-    16,24,32,40,48,56];
-
+%Load HMM MI data and correlation time
 HMM_MI =[];
 HMM_MI_shuffle = [];
-allchannellegend = cell(1,length(HMM_different_G));
-corr_t_legend = cell(1,length(HMM_different_G)+length(OU_different_G));
-for     G =1:length(HMM_different_G)
+HMM_peaks = [];
+for G =1:length(HMM_different_G) 
     load([HMM_former_name,num2str(HMM_different_G(G)),HMM_post_name ,'.mat'])
-    
+    HMM_peaks = [HMM_peaks;peak_times];
     HMM_MI = [HMM_MI;Mutual_infos];
     HMM_MI_shuffle = [HMM_MI_shuffle ;Mutual_shuffle_infos];
     allchannellegend{G} = ['G', num2str(HMM_different_G(G))];
-    corr_t_legend{G} = num2str(corr_time);
+    all_corr_t_legend{G} = [num2str(corr_time),' sec'];
+    corr_t_legend{G} = [num2str(corr_time),' sec'];
+end
+%Load OU MI data and correlation time
+if OU_or_Not
+    OU_MI =[];
+    OU_MI_shuffle = [];
+    for     G =1:length(OU_different_G) 
+        load([OU_former_name,num2str(OU_different_G(G)),OU_post_name ,'.mat'])
+
+        OU_MI = [OU_MI;Mutual_infos];
+        OU_MI_shuffle = [OU_MI_shuffle ;Mutual_shuffle_infos];
+        all_corr_t_legend{G+length(HMM_different_G)} = ['OU-', num2str(corr_time),' sec'];
+    end
 end
 
-OU_MI =[];
-OU_MI_shuffle = [];
-for     G =1:length(OU_different_G)
-    load([OU_former_name,num2str(OU_different_G(G)),OU_post_name ,'.mat'])
-    
-    OU_MI = [OU_MI;Mutual_infos];
-    OU_MI_shuffle = [OU_MI_shuffle ;Mutual_shuffle_infos];
-    corr_t_legend{G+length(HMM_different_G)} = ['OU-', num2str(corr_time)];
-end
-figure('units','normalized','outerposition',[0 0 1 1])
-ha = tight_subplot(8,8,[.04 .02],[0.07 0.02],[.02 .02]);
-
-
-for channelnumber=1:60 %choose file
-    axes(ha(rr(channelnumber)));
-    for     G =1:length(HMM_different_G)
-        
-        mean_MI_shuffle = mean(cell2mat(HMM_MI_shuffle(G,channelnumber)));
-        mutual_information = cell2mat(HMM_MI (G,channelnumber));
-        if channelnumber~=31
-            %             if max(mutual_information)-min(mutual_information)<0.2
-            %                 continue;
-            %             end
+%% Plot all channels
+if all_or_Not
+    figure('units','normalized','outerposition',[0 0 1 1])
+    ha = tight_subplot(8,8,[.04 .02],[0.07 0.02],[.02 .02]);
+    for channelnumber=1:60 %choose file
+        axes(ha(rr(channelnumber)));
+        %Plot HMM
+        for G =1:length(HMM_different_G)
+            mean_MI_shuffle = mean(cell2mat(HMM_MI_shuffle(G,channelnumber)));
+            mutual_information = cell2mat(HMM_MI (G,channelnumber));
+            if channelnumber~=31
             if max(mutual_information-mean_MI_shuffle)<0.1
                 continue;
             end
-        else
+            else
+                plot(time,mutual_information-mean_MI_shuffle); hold on; %,'color',cc(z,:));hold on
+                xlim([ -2300 1300])
+                ylim([0 100])
+                continue;
+            end
             plot(time,mutual_information-mean_MI_shuffle); hold on; %,'color',cc(z,:));hold on
-            xlim([ -1500 2000])
-            ylim([0 100])
-            continue;
+            xlim([ -2300 1300])
+            ylim([0 inf+0.1])
+            title(channelnumber)
         end
-        plot(time,mutual_information-mean_MI_shuffle); hold on; %,'color',cc(z,:));hold on
-        xlim([ -2300 1300])
-        ylim([0 inf+0.1])
-        
-    end
-    
-    if channelnumber == 31
-        lgd =legend(allchannellegend,'Location','north');
-        lgd.FontSize = 11;
-        legend('boxoff')
-    end
-    
-    hold off;
-    
-end
-% [25,33,34]
-%[52,53,57,58,59,60]
-%% Chose by hand and plot single channels 
 
-for channelnumber=[33 34]
-    figure(channelnumber)
-    for   G =1:length(HMM_different_G)
+        if channelnumber == 31
+            lgd =legend(corr_t_legend,'Location','northwest');
+            lgd.FontSize = 11;
+            legend('boxoff')
+        end
+
+        hold off;
+    end
+    %Save fig
+    set(gcf,'units','normalized','outerposition',[0 0 1 1])
+    fig =gcf;
+    fig.PaperPositionMode = 'auto';
+    fig.InvertHardcopy = 'off';
+    if save_photo
+    if OU_or_Not
+        saveas(fig,[exp_folder,'\FIG\sort\',OU_filename(1:end-3),'.tif'])
+    else
+        saveas(fig,[exp_folder,'\FIG\unsort\',HMM_filename(1:end-3),'.tif'])
+    end
+    end
+end
+%% Plot single channel
+mean_peaks = zeros(length(HMM_different_G),length(roi));
+for channelnumber= roi 
+     figure(channelnumber)
+     for   G =1:length(HMM_different_G)
+        mean_peaks(G,find(roi==channelnumber)) = HMM_peaks(G,channelnumber);
         mean_MI_shuffle = mean(cell2mat(HMM_MI_shuffle(G,channelnumber)));
         mutual_information = cell2mat(HMM_MI (G,channelnumber));hold on;
-        plot(time,smooth(mutual_information-mean_MI_shuffle),'LineWidth',1.5,'LineStyle','-');
+        %Plot HMM with different line
+        if G >=4
+            plot(time,smooth(mutual_information-mean_MI_shuffle)); 
+        elseif G >2
+            plot(time,smooth(mutual_information-mean_MI_shuffle),'k:'); 
+        else
+            plot(time,smooth(mutual_information-mean_MI_shuffle),'.'); 
+        end
         xlim([ -2300 1300])
         ylim([0 inf+0.1])
-    end
-    for   G =1:length(OU_different_G)
-        mean_MI_shuffle = mean(cell2mat(OU_MI_shuffle(G,channelnumber)));
-        mutual_information = cell2mat(OU_MI (G,channelnumber));hold on;
-        plot(time,smooth(mutual_information-mean_MI_shuffle),'LineWidth',1.5,'LineStyle',':');
-        xlim([ -1300 1300])
-        ylim([0 inf+0.1])
-    end
-    lgd =legend(corr_t_legend,'Location','northwest');
-    lgd.FontSize = 11;
-    legend('boxoff')
-    grid on
-    hold off;
+     end
+     %Plot OU
+     if OU_or_Not
+         for   G =1:length(OU_different_G)
+            mean_MI_shuffle = mean(cell2mat(OU_MI_shuffle(G,channelnumber)));
+            mutual_information = cell2mat(OU_MI (G,channelnumber));hold on;
+            plot(time,smooth(mutual_information-mean_MI_shuffle),'-.'); 
+            xlim([ -1300 1300])
+            ylim([0 inf+0.1])
+         end
+     end
+     grid on
+     
+     if OU_or_Not
+       lgd =legend(all_corr_t_legend,'Location','northwest');
+     else
+       lgd =legend(corr_t_legend,'Location','northwest');
+     end
+     lgd.FontSize = 11;
+     legend('boxoff')
+     hold off;
+     if save_photo
+         if sorted
+            if OU_or_Not
+                saveas(fig,[exp_folder,'\FIG\sort\',OU_filename,int2str(channelnumber),'.tif'])
+            else
+                saveas(fig,[exp_folder,'\FIG\sort\',HMM_filename,int2str(channelnumber),'.tif'])
+            end
+         else
+            if OU_or_Not
+                saveas(fig,[exp_folder,'\FIG\unsort\',OU_filename,int2str(channelnumber),'.tif'])
+            else
+                saveas(fig,[exp_folder,'\FIG\unsort\',HMM_filename,int2str(channelnumber),'.tif'])
+            end
+         end
+     end
 end
+% figure(100)
+% std_peaks = std(mean_peaks,0,2);
+% means_peaks = mean(mean_peaks,2);
+% errorbar([1.08,0.8,0.4833,0.32,0.18],means_peaks ,std_peaks,'o','color','k')
+% xlabel('Correlation time (sec)')
+% ylabel('Peak time shift (ms)')
