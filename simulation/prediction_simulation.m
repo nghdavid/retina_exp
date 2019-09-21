d183ef8cfe442ac688da79ef9b3b677f34692483
nghdavid <35591407+nghdavid@users.noreply.github.com>
2019-09-21 21:58:59 +0800

    Update code

--- simulation/prediction_simulation.m
+++ simulation/prediction_simulation.m
@@ -1,12 +1,16 @@
-%% Setup bar position and HMM trajectory
-
-bar_size = 7;
-G_HMM = 9;
-T = 1000;
+clear all;
+close all;
+%% Setup bar position and trajectory
+HMM = 1;
+T = 20000;
 dt=1/60;
+bar_size = 5;
+stimulus_range = 200;
+if HMM
+%% HMM trajectory
+G_HMM = 9;
 D_HMM = 2700000; %dynamical range
 omega =G_HMM/2.12;   % omega = G/(2w)=1.06; follow Bielak's overdamped dynamics/ 2015PNAS
-stimulus_range = 200;
 Xarray = zeros(1,T);
 Xarray(1,1)=0; % since the mean value of damped eq is zero
 Vx = zeros(1,T);
@@ -15,12 +19,30 @@ for t = 1:T-1
     Xarray(t+1) = Xarray(t) + Vx(t)*dt;
     Vx(t+1) = (1-G_HMM*dt)*Vx(t) - omega^2*Xarray(t)*dt + sqrt(dt*D_HMM)*randn;
 end
-
-nrx= (stimulus_range-bar_size*2)/(max(Xarray)-min(Xarray));
+nrx= (stimulus_range-bar_size*2-1)/(max(Xarray)-min(Xarray));
 Xarray2=Xarray*nrx;
 Xarray3=Xarray2+bar_size+1-min(Xarray2);%rearrange the boundary values
 newXarray=round(Xarray3);
 
+
+else
+%% OU trajectory
+G_OU = 2.45;
+D_OU = 2700000; %dynamical range
+omega =G_OU/2.12;   % omega = G/(2w)=1.06; follow Bielak's overdamped dynamics/ 2015PNAS
+x = zeros(1,T);
+x(1,1)=0; % since the mean value of damped eq is zero
+for uu = 1:T-1
+    x(uu+1) = (1-dt*G_OU/(2.12)^2)*x(uu)+sqrt(dt*D_OU)*randn;
+end
+% Normalize to proper moving range
+nrx=(stimulus_range-bar_size*2-1)/(max(x)-min(x));
+x2=x*nrx;
+x3=x2+bar_size+1-min(x2);%rearrange the boundary values
+newXarray=round(x3);
+
+
+end
 
 bar_position = zeros(T,stimulus_range);
 
@@ -28,17 +50,22 @@ for step = 1:T
    bar_position(step,newXarray(step)-bar_size:newXarray(step)+bar_size) = ones(1,2*bar_size+1); 
 end
 
+% figure(100)
+% plot(1:T,newXarray);
+
+
 %% Receptive field center surround filter
 
 sigma = 10;
 range = 1:stimulus_range;
-On_center = gaussmf(range,[sigma stimulus_range/2]);
-Off_center = -0.3*gaussmf(range,[sigma*3 stimulus_range/2]);
+On_center = 4*gaussmf(range,[sigma*3 stimulus_range/2]);
+Off_surround = -1*gaussmf(range,[sigma stimulus_range/2]);
 % figure(1)
 % plot(range,On_center);hold on
-% plot(range,Off_center);hold off
+% plot(range,Off_surround);hold off
 % figure(2)
-% plot(range,On_center+Off_center);
+% plot(range,On_center+Off_surround);
+
 
 %% On and Off temporal filter
 integration_time = 50;
@@ -46,12 +73,155 @@ size_curve = 10;
 On_temporal = zeros(1,integration_time);
 Off_temporal = zeros(1,integration_time);
 x = linspace(-1.0, 1.0, size_curve);
-curve = -x.*x+1;
+curve = -0.1*x.*x+1;
 On_temporal(5:5+size_curve-1) = On_temporal(5:5+size_curve-1)+curve;
-Off_temporal(10:10+size_curve-1) = Off_temporal(5:5+size_curve-1)+curve;
+Off_temporal(9:9+size_curve-1) = Off_temporal(5:5+size_curve-1)+curve;
 figure(3)
 plot(1:integration_time,On_temporal);hold on
 plot(1:integration_time,Off_temporal);hold off
 xlabel('time before spike')
 ylabel('weight')
-%% 
+
+
+%% Convolution of stimulus and receptive field
+conv_on = zeros(1,T);
+conv_off = zeros(1,T);
+for t = 1:T
+    conv_on(t) = sum(bar_position(t,:).*On_center);
+    conv_off(t) = sum(bar_position(t,:).*Off_surround);
+end
+% figure(4)
+% plot(1:T,conv_on);hold on
+% plot(1:T,conv_off);hold on
+% plot(1:T,normalize(newXarray),'k.');hold off
+% xlabel('time')
+% ylabel('Temporal change of center and surround')
+% title('HMM trajectory and temporal change after Gaussian filter')
+% legend('on','off','trajectory')
+
+
+%% Temporal filtering
+temp_on =zeros(1,T-integration_time);
+temp_off =zeros(1,T-integration_time);
+for t = 1:T-integration_time
+    temp_on(t) = sum(conv_on(t:t+integration_time-1).*fliplr(On_temporal));
+    temp_off(t) = sum(conv_off(t:t+integration_time-1).*fliplr(Off_temporal));
+end
+% 
+% 
+% figure(5)
+% plot(1:T-integration_time,normalize(temp_on));hold on
+% plot(integration_time+1:T,normalize(conv_on(integration_time+1:T)))
+% xlabel('time')
+% ylabel('Temporal change of ON')
+% title('On temporal change before and after temporal filter')
+% legend('After filter','Before filter')
+% set(gcf,'units','normalized','outerposition',[0 0 1 1])
+% 
+% figure(6)
+% plot(1:T-integration_time,normalize(temp_off));hold on
+% plot(integration_time+1:T,normalize(conv_off(integration_time+1:T)))
+% title('OFF temporal change before and after temporal filter')
+% ylabel('Temporal change of Off')
+% xlabel('time')
+% legend('After filter','Before filter')
+% set(gcf,'units','normalized','outerposition',[0 0 1 1])
+
+
+%% Subtraction of ON and OFF
+ratio_on = 1;
+ratio_off = 2;
+Subtraction = ratio_on*temp_on + ratio_off*temp_off;
+% figure(7)
+% plot(1:T-integration_time,normalize(Subtraction));hold on;
+% plot(integration_time+1:T,normalize(newXarray(integration_time+1:T)),'k.');hold off
+% xlabel('time')
+% ylabel('Subtraction of ON and OFF')
+% title('Effect of subtraction')
+% legend('Subtract','trajectory')
+% set(gcf,'units','normalized','outerposition',[0 0 1 1])
+% 
+% figure(8)
+% plot(integration_time+1:T,temp_on);hold on;
+% plot(integration_time+1:T,temp_off);hold on;
+% plot(1:T-integration_time,Subtraction);hold off;
+% xlabel('time')
+% ylabel('Volt')
+% title('Comparison of ON and OFF')
+% legend('on','off','subtraction')
+% set(gcf,'units','normalized','outerposition',[0 0 1 1])
+
+
+%% Calculation of MI
+TheStimuli = newXarray(integration_time+1:T);
+StimuSN=30; %number of stimulus states
+nX=sort(TheStimuli);
+abin=length(nX)/StimuSN;
+intervals=[nX(1:abin:end) inf]; % inf: the last term: for all rested values
+temp=0; isi2=[];
+for jj=1:length(TheStimuli)
+    temp=temp+1;
+    isi2(temp) = find(TheStimuli(jj)<=intervals,1);
+end
+TheStimuli = Subtraction;
+backward = 100;
+forward = 100;
+BinningInterval =1;
+nX=sort(temp_on);
+abin=length(nX)/StimuSN;
+intervals=[nX(1:abin:end) inf]; % inf: the last term: for all rested values
+temp=0; Neurons=[];
+for jj=1:length(TheStimuli)
+    temp=temp+1;
+    Neurons(temp) = find(TheStimuli(jj)<=intervals,1);
+end
+
+dat=[];informationp=[];temp=backward+2;
+for i=1:backward+1 %past(t<0)
+    x = Neurons((i-1)+forward+1:length(Neurons)-backward+(i-1))';
+    y = isi2(forward+1:length(isi2)-backward)';
+    dat{i}=[x,y];
+    norm=BinningInterval;
+
+    [N,C]=hist3(dat{i},[max(Neurons)+1,max(isi2)]); %20:dividing firing rate  6:# of stim
+    px=sum(N,1)/sum(sum(N)); 
+    py=sum(N,2)/sum(sum(N)); 
+    pxy=N/sum(sum(N));
+    temp2=[];
+    for j=1:length(px)
+        for k=1:length(py)
+          temp2(k,j)=pxy(k,j)*log( pxy(k,j)/ (py(k)*px(j)) )/log(2)/norm;
+        end
+    end
+    temp=temp-1;
+    informationp(temp)=nansum(temp2(:));
+
+end  
+
+dat=[];informationf=[];temp=0;sdat=[];
+for i=1:forward
+    x =Neurons(forward+1-i:length(Neurons)-backward-i)';
+    y = isi2(forward+1:length(isi2)-backward)';
+    dat{i}=[x,y];
+    norm=BinningInterval;
+
+    [N,C]=hist3(dat{i},[max(Neurons)+1,max(isi2)]); %20:dividing firing rate  6:# of stim
+    px=sum(N,1)/sum(sum(N)); 
+    py=sum(N,2)/sum(sum(N)); 
+    pxy=N/sum(sum(N));
+    temp2=[];
+    for j=1:length(px)
+        for k=1:length(py)
+            temp2(k,j)=pxy(k,j)*log( pxy(k,j)/ (py(k)*px(j)) )/log(2)/norm;
+        end
+    end
+    temp=temp+1;
+    informationf(temp)=nansum(temp2(:));
+
+end
+figure(9)
+information=[informationp informationf];
+time=[-backward:forward];
+plot(time,information);
+xlabel('time shift')
+ylabel('MI(bits)')
