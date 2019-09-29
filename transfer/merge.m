74f840ea6103b0d53769f7649fae7269ee7d8640
nghdavid <35591407+nghdavid@users.noreply.github.com>
2019-09-29 17:09:47 +0800

    Arrange transfer

--- transfer/merge.m
+++ transfer/merge.m
@@ -2,28 +2,39 @@ clear all;
 close all;
 
 code_folder = pwd;
-exp_folder = 'E:\0709';
+exp_folder = 'D:\20190916';
 cd(exp_folder)
 cd playmovie
-fileID = fopen('playmovie/list.txt','r');
+fileID = fopen('playmovie/all_list.txt','r');
 
-formatSpec = '%c';
+
 txt = textscan(fileID,'%s','delimiter','\n'); 
-num_files = length(txt{1});
+num_files = length(txt{1})-1;
 
 merge_ID = fopen('playmovie/merge_list.txt','r');
-formatSpec = '%c';
 merge_txt = textscan(merge_ID,'%s','delimiter','\n'); 
 cd ..
 mkdir merge
 
 
-for m = 6%:num_files
-    name = txt{1}{m};
+for m = 1:num_files
+    name = txt{1}{m+1};
     workspace_name = merge_txt{1}{m};
     if length(name) < 6
         disp([name,' has an error or it is spontaneous'])
         continue;
+    end
+    if length(name)>13
+        if strcmp(name(9:13),'ONOFF')
+            continue;
+        elseif strcmp(name(6:12),'Grating')
+            continue;
+        end
+    end
+    if length(name)>17
+        if strcmp(name(14:18),'OnOff')
+            continue;
+        end
     end
     if strcmp(name(6),'H')
         type = 'HMM';
@@ -37,13 +48,15 @@ for m = 6%:num_files
         type = 'Reveral';
     elseif strcmp(name(6),'C')
         type = 'Checker';
+    elseif  strcmp(name(1:2),'cs')
+        continue
     else
         type = 'else';
    
         
     end
     
-    pass = 0;
+    
     cd(code_folder)
     pass = reconstruct(exp_folder,type,name,workspace_name);
     if pass
