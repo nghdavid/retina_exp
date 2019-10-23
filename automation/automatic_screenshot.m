7470e374e3458c713d9b38485574a409c2c1b428
nghdavid <35591407+nghdavid@users.noreply.github.com>
2019-10-24 01:26:22 +0800

    Code for screenshot of McRack recording

---
+++ automation/automatic_screenshot.m
@@ -0,0 +1,55 @@
+%% This code can take screenshot in each period and then save photo
+%After saving photo, it send to assigned email
+close all;
+clear all;
+%% Go to photo directory
+recording_time = 8;%Hours
+time_cycle = 300;%second (5 mins)
+pos = [0 0 400 400];% Screen position [left top width height]
+photo_folder = 'C:\Users\nghdavid\Desktop\test';
+rmdir(photo_folder,'s');%Remove whole directory
+mkdir(photo_folder)%Make new directory
+cd(photo_folder)
+%%  Email parameters
+mail = 'hydrolab320@gmail.com'; % my gmail address
+password = 'yourpassword';  % my gmail password 
+host = 'smtp.gmail.com';
+reciever_gmail = 'nghdavid123@gmail.com';
+%Remember to close down antivirus software and turn off security in gmail
+%% preferences
+setpref('Internet','SMTP_Server', host);
+setpref('Internet','E_mail',mail);
+setpref('Internet','SMTP_Username',mail);
+setpref('Internet','SMTP_Password',password);
+props = java.lang.System.getProperties;
+props.setProperty('mail.smtp.auth','true');
+props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
+props.setProperty('mail.smtp.socketFactory.port','465');
+
+time = 0;%Counting time
+num = 1;%Counting numbers
+%% Run a cycle until time is up
+while time < recording_time*3600%Check time is up
+    %% Take screenshot
+    robot = java.awt.Robot();
+    rect = java.awt.Rectangle(pos(1),pos(2),pos(3),pos(4));
+    cap = robot.createScreenCapture(rect);
+    rgb = typecast(cap.getRGB(0,0,cap.getWidth,cap.getHeight,[],0,cap.getWidth),'uint8');
+    imgData = zeros(cap.getHeight,cap.getWidth,3,'uint8');
+    imgData(:,:,1) = reshape(rgb(3:4:end),cap.getWidth,[])';
+    imgData(:,:,2) = reshape(rgb(2:4:end),cap.getWidth,[])';
+    imgData(:,:,3) = reshape(rgb(1:4:end),cap.getWidth,[])';
+    imwrite(imgData,[num2str(num),'.jpeg'])
+    %% load photo
+    all_file = dir('*.jpeg') ; % change the type of the files which you want to select, subdir or dir. 
+    n_file = length(all_file) ;
+    file = all_file(n_file).name ;
+    [pathstr, name, ext] = fileparts(file);
+    filename = [name,ext];
+
+    %%  Send the email
+    sendmail(reciever_gmail,'Mc_rack recording screen shot',['Time is ',all_file(n_file).date],filename);
+    pause(time_cycle)%Wait for five minutes
+    time = time + time_cycle;
+    num = num + 1;
+end
