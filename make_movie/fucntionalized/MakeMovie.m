a7d2cb2abd5f2cd0f1734dffc3219337696400c1
nghdavid <35591407+nghdavid@users.noreply.github.com>
2019-12-02 15:57:25 +0800

    Easy way to  make movie

--- make_movie/fucntionalized/MakeMovie.m
+++ make_movie/fucntionalized/MakeMovie.m
@@ -1,95 +1,100 @@
 clear all;
 makemovie_folder = 'E:\makemovie';
-date = '0421';
+date = '1202';
 seed_date = '0421';
-cd(makemovie_folder);
-load('calibrate_pt.mat')%Load dotPositionMatrix
-load('screen_brightness.mat')%Load screen_brightness
-load('boundary_set.mat')
 
+movie_folder = '\\DESKTOP-JMQPLEJ\temp_movie\videos\';
+cd(movie_folder)
+mkdir HMM
+mkdir OU
+videoworkspace_folder = '\\DESKTOP-JMQPLEJ\temp_movie\videoworkspace\';
+cd(videoworkspace_folder)
+mkdir HMM
+mkdir OU
+cd(makemovie_folder);
 %% HMM 5min part
-makeHMMvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,5);
-makeHMMvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,5);
-makeHMMvideo(makemovie_folder, pi/4, 'UL_DR', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,5);
-makeHMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,5);
-makeDarkHMMvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,5);
-makeDarkHMMvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,5);
-makeDarkHMMvideo(makemovie_folder, pi/4, 'UL_DR', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,5);
-makeDarkHMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,5);
+makeHMMvideo(makemovie_folder, 0, 'RL', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,5);
+makeHMMvideo(makemovie_folder, pi/2, 'UD', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,5);
+makeHMMvideo(makemovie_folder, pi/4, 'UL_DR', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,5);
+makeHMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,5);
+makeDarkHMMvideo(makemovie_folder, 0, 'RL', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,5);
+makeDarkHMMvideo(makemovie_folder, pi/2, 'UD', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,5);
+makeDarkHMMvideo(makemovie_folder, pi/4, 'UL_DR', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,5);
+makeDarkHMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,5);
 
 %% HMM 7 min part
-makeHMMvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,7);
-makeHMMvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,7);
-makeHMMvideo(makemovie_folder, pi/4, 'UL_DR', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,7);
-makeHMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,7);
-makeDarkHMMvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,7);
-makeDarkHMMvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,7);
-makeDarkHMMvideo(makemovie_folder, pi/4, 'UL_DR', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,7);
-makeDarkHMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,7);
+makeHMMvideo(makemovie_folder, 0, 'RL', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,7);
+makeHMMvideo(makemovie_folder, pi/2, 'UD', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,7);
+makeHMMvideo(makemovie_folder, pi/4, 'UL_DR', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,7);
+makeHMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,7);
+makeDarkHMMvideo(makemovie_folder, 0, 'RL', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,7);
+makeDarkHMMvideo(makemovie_folder, pi/2, 'UD', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,7);
+makeDarkHMMvideo(makemovie_folder, pi/4, 'UL_DR', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,7);
+makeDarkHMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,7);
 
 %% Long HMM 5min part
-makeLongHMMvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,5);
-makeLongHMMvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,5);
-makeLongDarkHMMvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,5);
-makeLongDarkHMMvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,5);
+makeLongHMMvideo(makemovie_folder, 0, 'RL', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,5);
+makeLongHMMvideo(makemovie_folder, pi/2, 'UD', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,5);
+makeLongDarkHMMvideo(makemovie_folder, 0, 'RL', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,5);
+makeLongDarkHMMvideo(makemovie_folder, pi/2, 'UD', [movie_folder,'HMM'],[videoworkspace_folder,'HMM'], seed_date,date,5);
 
 %% Long HMM 7 min part
-makeLongHMMvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,7);
-makeLongHMMvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,7);
-makeLongDarkHMMvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,7);
-makeLongDarkHMMvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace\HMM', seed_date,date,7);
+makeLongHMMvideo(makemovie_folder, 0, 'RL', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,7);
+makeLongHMMvideo(makemovie_folder, pi/2, 'UD', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,7);
+makeLongDarkHMMvideo(makemovie_folder, 0, 'RL', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,7);
+makeLongDarkHMMvideo(makemovie_folder, pi/2, 'UD', [movie_folder,'HMM'], [videoworkspace_folder,'HMM'], seed_date,date,7);
 
 %% OU part
-makeOUvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\OU', 'D:\0930v\videoworkspace\OU', date);
-makeOUvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\OU', 'D:\0930v\videoworkspace\OU', date);
-makeOUvideo(makemovie_folder, pi/4, 'UL_DR', 'D:\0930v\videos\OU', 'D:\0930v\videoworkspace\OU', date);
-makeOUvideo(makemovie_folder, 3*pi/4, 'UR_DL', 'D:\0930v\videos\OU', 'D:\0930v\videoworkspace\OU', date);
-makeDarkOUvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\OU', 'D:\0930v\videoworkspace\OU', date);
-makeDarkOUvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\OU', 'D:\0930v\videoworkspace\OU', date);
-makeDarkOUvideo(makemovie_folder, pi/4, 'UL_DR', 'D:\0930v\videos\OU', 'D:\0930v\videoworkspace\OU', date);
-makeDarkOUvideo(makemovie_folder, 3*pi/4, 'UR_DL', 'D:\0930v\videos\OU', 'D:\0930v\videoworkspace\OU', date);
+makeOUvideo(makemovie_folder, 0, 'RL', [movie_folder,'OU'], [videoworkspace_folder,'OU'], date);
+makeOUvideo(makemovie_folder, pi/2, 'UD', [movie_folder,'OU'], [videoworkspace_folder,'OU'], date);
+makeOUvideo(makemovie_folder, pi/4, 'UL_DR', [movie_folder,'OU'], [videoworkspace_folder,'OU'], date);
+makeOUvideo(makemovie_folder, 3*pi/4, 'UR_DL', [movie_folder,'OU'], [videoworkspace_folder,'OU'], date);
+makeDarkOUvideo(makemovie_folder, 0, 'RL', [movie_folder,'OU'], [videoworkspace_folder,'OU'], date);
+makeDarkOUvideo(makemovie_folder, pi/2, 'UD',[movie_folder,'OU'], [videoworkspace_folder,'OU'], date);
+makeDarkOUvideo(makemovie_folder, pi/4, 'UL_DR', [movie_folder,'OU'], [videoworkspace_folder,'OU'], date);
+makeDarkOUvideo(makemovie_folder, 3*pi/4, 'UR_DL', [movie_folder,'OU'], [videoworkspace_folder,'OU'], date);
 
 %% Long OU part
-makeLongOUvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\OU', 'D:\0930v\videoworkspace\OU', date);
-makeLongOUvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\OU', 'D:\0930v\videoworkspace\OU', date);
-makeLongDarkOUvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\OU', 'D:\0930v\videoworkspace\OU', date);
-makeLongDarkOUvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\OU', 'D:\0930v\videoworkspace\OU', date);
+makeLongOUvideo(makemovie_folder, 0, 'RL', [movie_folder,'OU'],[videoworkspace_folder,'OU'], date);
+makeLongOUvideo(makemovie_folder, pi/2, 'UD', [movie_folder,'OU'],[videoworkspace_folder,'OU'], date);
+makeLongDarkOUvideo(makemovie_folder, 0, 'RL', [movie_folder,'OU'], [videoworkspace_folder,'OU'], date);
+makeLongDarkOUvideo(makemovie_folder, pi/2, 'UD',[movie_folder,'OU'],[videoworkspace_folder,'OU'], date);
 
 %% Short HMM part
-makeshort_HMMvideo(makemovie_folder, 0, 'RL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace', date);
-makeshort_HMMvideo(makemovie_folder, pi/2, 'UD', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace', date);
-makeshort_HMMvideo(makemovie_folder, pi/4, 'UL_DR', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace', date);
-makeshort_HMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', 'D:\0930v\videos\HMM', 'D:\0930v\videoworkspace', date);
+makeshort_HMMvideo(makemovie_folder, 0, 'RL', [movie_folder,'HMM'], videoworkspace_folder, date);
+makeshort_HMMvideo(makemovie_folder, pi/2, 'UD', [movie_folder,'HMM'], videoworkspace_folder, date);
+makeshort_HMMvideo(makemovie_folder, pi/4, 'UL_DR', [movie_folder,'HMM'], videoworkspace_folder, date);
+makeshort_HMMvideo(makemovie_folder, 3*pi/4, 'UR_DL', [movie_folder,'HMM'], videoworkspace_folder, date);
 
 %% Short HMM Rona part
-makeRona_short_HMMvideo(makemovie_folder, 'D:\0930v\videos', 'D:\0930v\videoworkspace', date);
+makeRona_short_HMMvideo(makemovie_folder,movie_folder, videoworkspace_folder, date);
 
 %% Gollisch onoff part
-Gollish_OnOff_movie(makemovie_folder, 'D:\0930v\videos',date,0.5);
-Gollish_OnOff_movie(makemovie_folder, 'D:\0930v\videos',date,0.8);
+Gollish_OnOff_movie(makemovie_folder, movie_folder,date,0.5);
+Gollish_OnOff_movie(makemovie_folder, movie_folder,date,0.8);
 
 %% Checkerboard
-checkerboard(makemovie_folder,'D:\0930v\videos', 'D:\0930v\videoworkspace',20,13,date,5);
-checkerboard(makemovie_folder,'D:\0930v\videos', 'D:\0930v\videoworkspace',30,13,date,5);
-checkerboard(makemovie_folder,'D:\0930v\videos', 'D:\0930v\videoworkspace',20,15,date,5);
-checkerboard(makemovie_folder,'D:\0930v\videos', 'D:\0930v\videoworkspace',30,15,date,5);
+checkerboard(makemovie_folder,movie_folder, videoworkspace_folder,20,13,date,5);
+checkerboard(makemovie_folder,movie_folder, videoworkspace_folder,30,13,date,5);
+checkerboard(makemovie_folder,movie_folder, videoworkspace_folder,20,15,date,5);
+checkerboard(makemovie_folder,movie_folder, videoworkspace_folder,30,15,date,5);
 
 %% Grating
-makeGratingvideo(makemovie_folder,'D:\0930v\videos', 300, 0.75,5,date);
-makeGratingvideo(makemovie_folder,'D:\0930v\videos', 600, 0.75,5,date);
-makeGratingvideo(makemovie_folder,'D:\0930v\videos', 300, 0.375,5,date);
-makeGratingvideo(makemovie_folder,'D:\0930v\videos', 300, 1.5,5,date);
-makeGratingvideo(makemovie_folder,'D:\0930v\videos', 600, 0.375,5,date);
-makeGratingvideo(makemovie_folder,'D:\0930v\videos', 600, 1.5,5,date);
+makeGratingvideo(makemovie_folder,movie_folder, 300, 0.75,5,date);
+makeGratingvideo(makemovie_folder,movie_folder, 600, 0.75,5,date);
+makeGratingvideo(makemovie_folder,movie_folder, 300, 0.375,5,date);
+makeGratingvideo(makemovie_folder,movie_folder, 300, 1.5,5,date);
+makeGratingvideo(makemovie_folder,movie_folder, 600, 0.375,5,date);
+makeGratingvideo(makemovie_folder,movie_folder, 600, 1.5,5,date);
 
-makeGratingvideo(makemovie_folder,'D:\0930v\videos', 300, 0.75,8,date);
-makeGratingvideo(makemovie_folder,'D:\0930v\videos', 600, 0.75,8,date);
-makeGratingvideo(makemovie_folder,'D:\0930v\videos', 300, 0.375,8,date);
-makeGratingvideo(makemovie_folder,'D:\0930v\videos', 300, 1.5,8,date);
-makeGratingvideo(makemovie_folder,'D:\0930v\videos', 600, 0.375,8,date);
-makeGratingvideo(makemovie_folder,'D:\0930v\videos', 600, 1.5,8,date);
+makeGratingvideo(makemovie_folder,movie_folder, 300, 0.75,8,date);
+makeGratingvideo(makemovie_folder,movie_folder, 600, 0.75,8,date);
+makeGratingvideo(makemovie_folder,movie_folder, 300, 0.375,8,date);
+makeGratingvideo(makemovie_folder,movie_folder, 300, 1.5,8,date);
+makeGratingvideo(makemovie_folder,movie_folder, 600, 0.375,8,date);
+makeGratingvideo(makemovie_folder,movie_folder, 600, 1.5,8,date);
 %% Reversal bar
-makeREvideo(makemovie_folder,0, 'RL','D:\0930v\videos', 'D:\0930v\videoworkspace',date,2.4);
-makeREvideo(makemovie_folder,pi/2, 'UD','D:\0930v\videos', 'D:\0930v\videoworkspace',date,2.4);
-makeREvideo(makemovie_folder,pi/4, 'UL_DR','D:\0930v\videos', 'D:\0930v\videoworkspace',date,2.4);
-makeREvideo(makemovie_folder,3*pi/4, 'UR_DL','D:\0930v\videos', 'D:\0930v\videoworkspace',date,2.4);
+makeREvideo(makemovie_folder,0, 'RL',movie_folder, videoworkspace_folder,date,2.4);
+makeREvideo(makemovie_folder,pi/2, 'UD',movie_folder, videoworkspace_folder,date,2.4);
+makeREvideo(makemovie_folder,pi/4, 'UL_DR',movie_folder, videoworkspace_folder,date,2.4);
+makeREvideo(makemovie_folder,3*pi/4, 'UR_DL',movie_folder, videoworkspace_folder,date,2.4);
