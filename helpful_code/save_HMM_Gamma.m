69a2cd159fa8e188bd5883540864f4d406ed65cd
nghdavid <35591407+nghdavid@users.noreply.github.com>
2020-03-27 16:10:02 +0800

    Code for saving parameters of each experiment

--- helpful_code/save_HMM_Gamma.m
+++ helpful_code/save_HMM_Gamma.m
@@ -2,9 +2,12 @@ close all;
 clear all;
 code_folder = pwd;
 exp_folder = 'E:\20200302';
+HMM_date1 = '0224';
+HMM_date2 = '0225';
 cd(exp_folder)
 direction = 'UL_DR';
 HMM_different_G = [3,4.5,7.5,12,20];
+edge_different_G = [3,4.5,7.5,12,20];
 mins = 5;%Length of movie length
 OU_date = '0224';
 OU_different_G = [3,20];
