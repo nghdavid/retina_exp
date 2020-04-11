9650385ea4113f044e78e9199b01e7217006e5a2
nghdavid <35591407+nghdavid@users.noreply.github.com>
2020-04-11 12:02:16 +0800

    Code for analysis of STA of bar

--- basic_analysis/Calculate_STA.m
+++ basic_analysis/Calculate_STA.m
@@ -2,14 +2,15 @@
 close all;
 clear all;
 code_folder = pwd;
-exp_folder = 'E:\20190721';
+exp_folder = 'E:\20190811';
 cd(exp_folder);
+
 mkdir STA
 cd sort_merge_spike\MI
 all_file = subdir('*.mat') ; % change the type of the files which you want to select, subdir or dir.
 n_file = length(all_file) ;
 unit = 1;
-
+velocity = 0;%speed is 1, position is 0
 forward = 90;%90 bins before spikes for calculating STA
 backward = 90;%90 bins after spikes for calculating STA
 
@@ -27,10 +28,12 @@ for z =1:n_file %choose file
     name=[name];
     z
     name
-    acf = autocorr(bin_pos,100);
-    corr_time = find(abs(acf-0.5) ==min(abs(acf-0.5)))/60;
     % put your stimulus here!!
-    TheStimuli=relative_pos;  %recalculated bar position
+    if velocity
+        TheStimuli = bin_pos;
+    else
+        TheStimuli= bin_pos;  %recalculated bar position
+    end
     time=[-forward :backward]*BinningInterval;
     % Binning
     bin=BinningInterval*10^3; %ms
@@ -38,15 +41,7 @@ for z =1:n_file %choose file
     
     %% BinningSpike and calculate STA
     BinningSpike = zeros(60,length(BinningTime));
-    analyze_spikes = cell(1,60);
-    
-    for i = 1:60  % i is the channel number
-        analyze_spikes{i} =[];
-        for u = unit
-            analyze_spikes{i} = [analyze_spikes{i} sorted_spikes{u,i}'];
-        end
-        analyze_spikes{i} = sort(analyze_spikes{i});
-    end
+    analyze_spikes = get_multi_unit(exp_folder,sorted_spikes,unit);
     sum_n = zeros(1,60);
     dis_STA = zeros(60,forward+backward+1);
     for i = 1:60  % i is the channel number
@@ -54,13 +49,19 @@ for z =1:n_file %choose file
         BinningSpike(i,:) = n ;
         sum_n(i) = sum_n(i)+ sum(BinningSpike(i,forward+1:length(BinningTime)-backward));
         for time_shift = forward+1:length(BinningTime)-backward
-            dis_STA(i,:) = dis_STA(i,:) + BinningSpike(i,time_shift)*TheStimuli(i,time_shift-forward:time_shift+backward);
+            dis_STA(i,:) = dis_STA(i,:) + BinningSpike(i,time_shift)*TheStimuli(time_shift-forward:time_shift+backward);
         end
         if sum_n(i)
             dis_STA(i,:) = dis_STA(i,:)/sum_n(i);
         end
+        
     end
-    
-    save([exp_folder,'\STA\',name(12:end),'.mat'],'time','dis_STA','relative_pos','corr_time')
+    acf = autocorr(bin_pos,100);
+    corr_time = find(abs(acf-0.5) ==min(abs(acf-0.5)))/60;
+    if velocity
+        save([exp_folder,'\STA\v_',name(12:end),'.mat'],'time','dis_STA','bin_pos','corr_time')
+    else
+        save([exp_folder,'\STA\pos_',name(12:end),'.mat'],'time','dis_STA','bin_pos','corr_time')
+    end
 end
 
