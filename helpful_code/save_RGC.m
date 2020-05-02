d789b313532be5127ea4726c2be7f2d4439fd321
nghdavid <35591407+nghdavid@users.noreply.github.com>
2020-05-02 16:34:01 +0800

    Update

--- helpful_code/save_RGC.m
+++ helpful_code/save_RGC.m
@@ -2,13 +2,13 @@ close all;
 clear all;
 load('rr.mat')
 code_folder = pwd;
-date = '20200306';
+date = '20200418';
 exp_folder = ['E:\',date];
 cd(exp_folder)
 load('Analyzed_data\sort\0224_Gollish_OnOff_movie_5min_Br50_Q100_6.5mW_re.mat')
 load('Analyzed_data\sort\0224_cSTA_wf_3min_Q100_re.mat')
-load('Analyzed_data\sort\RFcenter.mat')
-
+load('Analyzed_data\30Hz_27_RF_re\sort\RF_properties.mat')
+load('predictive_channel\bright_bar.mat')
 RGCs = cell(1,60);
 for channel = 1:60
     r = RGC;
@@ -20,11 +20,13 @@ for channel = 1:60
     if ~isnan(on_off_index(channel))
         r.onoff_index = on_off_index(channel);%Gollisch result
     end
-    if sum(RFcenter(channel,:)) ~= 0
-        r.center_RF = RFcenter(channel,:);
+%     if sum(RFcenter(channel,:)) ~= 0
+%         r.center_RF = RFcenter(channel,:);
+%     end
+    if sum(RF_properties(channel,:)) ~= 0
+        r.center_RF = [RF_properties(channel,2),RF_properties(channel,4)];
+        r.RF_size = RF_properties(channel,7);
     end
-    
-    load('predictive_channel\bright_bar.mat')
     if ismember(channel,p_channel)
         r.bright_bar_predictive = 1;
     elseif ismember(channel,np_channel)
