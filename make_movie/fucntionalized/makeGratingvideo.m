29480638f124cf09768b01e6d21e6cb7f3227aae
llincooi <33573023+llincooi@users.noreply.github.com>
2020-06-17 12:18:10 +0800

    Update makeGratingvideo.m

--- make_movie/fucntionalized/makeGratingvideo.m
+++ make_movie/fucntionalized/makeGratingvideo.m
@@ -1,11 +1,15 @@
-function makeGratingvideo(makemovie_folder,video_folder,date,calibration_date, background_lumin, bar_lumin)
+function makeGratingvideo(makemovie_folder,video_folder, videoworkspace_folder, date,calibration_date, background_lumin, bar_lumin)
 
 load(['C:\calibration\',calibration_date,'oled_calibration\calibration.mat'])
 load(['C:\calibration\',calibration_date,'oled_calibration\oled_boundary_set.mat']);
 % %             width   interval
 % % grating     300     300
 % % jittering   150     150
-rest_mean_lumin = interp1(real_lum,lum,(back_lumin+bar_lumin)/2,'linear');
+rest_mean_lumin = interp1(real_lum,lum,(background_lumin+bar_lumin)/2,'linear');
+OLED_bar_lumin = interp1(real_lum,lum,bar_lumin,'linear');
+OLED_background_lumin_lumin = interp1(real_lum,lum,background_lumin,'linear');
+background = zeros(1024,1280);
+background(leftx_bd:rightx_bd, lefty_bd:righty_bd) = OLED_background_lumin;
 Y =meaCenter_y;
 cd (video_folder)
 %% video setting
@@ -27,39 +31,40 @@ for l = 1:60*10
 end
 bar_real_width_set = [130 300 600];
 temporal_frequency_set = [3/16 ones(1,2)*3/8 ones(1,4)*3/4];
-theta_set = [[0 pi/4 pi/2 pi*3/4] -1*[pi pi/4 pi/2 pi*3/4]]  ;
+theta_set = [[0 pi/4 pi/2 pi*3/4] pi+[0 pi/4 pi/2 pi*3/4]];
 SET_I = 1:length(bar_real_width_set)*length(temporal_frequency_set)*length(theta_set);
+Order = [];
 cd(makemovie_folder)
 theta = pi*20;
 while isempty(SET_I)
-    last_theta = theta;
+    last_theta = theta2pi;
     ii = randi(length(SET_I));
     [i j k] = ind2sub([length(bar_real_width_set) length(temporal_frequency_set) length(theta_set)], SET_I(ii));
     bar_real_width = bar_real_width_set(i);
     temporal_frequency = temporal_frequency_set(j);
-    theta = abs(theta_set(k));
+    theta2pi = theta_set(k);
     if length(SET_I) == 1
-    elseif theta == last_theta
+    elseif theta2pi == last_theta
         continue
     end
-    if theta == pi
-        theta == 0;
-    end
-    SET_I(ii) = []
-        
-    %% Check each grating matrix have calculated or not
-    matrix_folder = ['C:\',calibration_date,'grating_matrix_',num2str(bar_lumin), '-', num2str(background_lumin),'mW_',num2str(bar_real_width) ,'micro_'];
-    if exist([matrix_folder,num2str(theta*4/pi)]) == 0
-        disp('Does not have matrix yet')
-        make_grating_matrix(calibration_date,mean_lumin,theta*4/pi,bar_real_width,temporal_frequency)
-    end
+    if theta2pi >= pi, theta = theta2pi-pi; end
+    Order = [Order SET_I(ii)];
+    SET_I(ii) = [];
     %% Calculate each bar
     if pi/4 <= theta && pi*3/4 >= theta
         longest_dis = mea_size_bm/sin(theta)+bar_interval;%the distance a bar would travel
     else
         longest_dis = abs(mea_size_bm/cos(theta))+bar_interval;
     end
-    bar_le = ceil(longest_dis/2);
+    bar_len = ceil(longest_dis/2);
+    bar_wid = round( (bar_real_width/micro_per_pixel-1)/2 );
+    bar_center = [meaCenter_x meaCenter_y];
+        %% Check each grating matrix have calculated or not
+    matrix_folder = ['C:\', calibration_date,'arbitrary_bar_location_', num2str(bar_wid),'x', num2str(bar_len), '_center(',num2str(bar_center(1)),',',num2str(bar_center(2)),')\'];
+    if exist([matrix_folder,num2str(theta*4/pi)]) == 0
+        disp('Does not have matrix yet')
+        locate_arbitrary_bar(calibration_date, bar_wid, bar_len, bar_center, theta/pi*4)
+    end
     num_bar = ceil(longest_dis/bar_interval);%number of bar in movie
     num_move = 400; %Number of steps that move %6.67s
     if temporal_frequency <= 3/16
@@ -68,7 +73,7 @@ while isempty(SET_I)
         num_move = num_move*2;
     end
     xarray = zeros(num_bar,num_move);%Array that store each bar postion(each row store each bar postions)
-    xarray(1,1) = randi(mea_size_bm);%leftx_bd+bar_wid+1;%Left bar first position %randomly
+    xarray(1,1) = randi(mea_size_bm)-1;%leftx_bd+bar_wid+1;%Left bar first position %randomly 0~mea_size_bm-1
     for i = 2:num_move
         xarray(1,i) = xarray(1,i-1)+temporal_frequency*bar_interval/60;
     end
@@ -78,17 +83,16 @@ while isempty(SET_I)
         end
     end
     xarray = mod(xarray , num_bar*bar_interval); % set all xarray into mea region
-    if theta_set(k) < 0
-        xarray = fliplr(xarray);%For reverse direction
-    end
+    if theta2pi >= pi, xarray = fliplr(xarray);  end%For reverse direction
     %% Plot each bar
     for kk =1:length(xarray)
         temp = zeros(1024,1280);%Initialize each frame
         for i = 1:num_bar%Plot each bar
-            X=round(xarray(i,kk));
+            X=round(xarray(i,kk))+leftx_bd;
             load([matrix_folder,num2str(theta*4/pi),'\',num2str(X),'.mat']);% Load picture matrix
-            temp = temp + a;
+            temp = temp + a*(OLED_bar_lumin - OLED_background_lumin);
         end
+        temp = temp + background;
         temp(500-35:500+35,1230:1280)=1;
         writeVideo(writerObj,temp);
     end
@@ -107,4 +111,5 @@ for mm=1:10
     writeVideo(writerObj,img);
 end
 close(writerObj);
+save([videoworkspace_folder,'/',name,'.mat'],'Order', 'bar_real_width_set', 'temporal_frequency_set', 'theta_set')
 end
